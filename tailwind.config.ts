a176121040df4a19d0be02d717001b270f6f8720
Neville Jeremy <ketjeh.developer@gmail.com>
2025-01-08 11:21:58 +0700

    feat: modify home page with reusable components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,21 @@ export default {
 				2: '#161925',
 			},
 			blue: {
-				1: '#0E78F9'
+				1: '#0E78F9',
+			},
+			sky: {
+				1: '#C9DDFF',
+				2: '#ECF0FF',
+				3: '#F5FCFF',
+			},
+			orange: {
+				1: '#FF742E',
+			},
+			purple: {
+				1: '#830EF9',
+			},
+			yellow: {
+				1: '#F9A90E'
 			},
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -58,6 +72,9 @@ export default {
   				'5': 'hsl(var(--chart-5))'
   			}
   		},
+		backgroundImage: {
+			hero: "url('/images/hero-background.png')"
+		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
